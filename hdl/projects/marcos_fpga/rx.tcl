51003e98061c00a18c2de313836c1939a8e74a7b
linkaishengmri <linkaishengchn@163.com>
2025-12-12 21:37:24 +0800

    use xilinx cic ip with dynamic shift

--- hdl/projects/marcos_fpga/rx.tcl
+++ hdl/projects/marcos_fpga/rx.tcl
@@ -72,54 +72,116 @@ cell xilinx.com:ip:axis_broadcaster:1.1 rate {
     aresetn rx_aresetn
 }
 
-if {$dsp_source=="OPENSOURCE"} {
-    cell open-mri:user:CIC:1.0 cic_real {
-	INP_DW 32
-	OUT_DW 32
-	RATE_DW 16
-	CIC_R 4095
-	CIC_N 6
-	CIC_M 1
-	PRUNE_BITS 0x480000004500000044000000440000004300000042000000410000003c00000031000000270000001c000000100000000400000000
-	VAR_RATE 1
-	EXACT_SCALING 0
-	PRG_SCALING 1
-    } {
-	S_AXIS_RATE rate/M00_AXIS
-	S_AXIS_IN comb2iq/M00_AXIS
-	clk /pll_0/clk_out1
-	reset_n rx_aresetn
-    }
+# if {$dsp_source=="OPENSOURCE"} {
+#     cell open-mri:user:CIC:1.0 cic_real {
+# 	INP_DW 32
+# 	OUT_DW 32
+# 	RATE_DW 16
+# 	CIC_R 4095
+# 	CIC_N 6
+# 	CIC_M 1
+# 	PRUNE_BITS 0x480000004500000044000000440000004300000042000000410000003c00000031000000270000001c000000100000000400000000
+# 	VAR_RATE 1
+# 	EXACT_SCALING 0
+# 	PRG_SCALING 1
+#     } {
+# 	S_AXIS_RATE rate/M00_AXIS
+# 	S_AXIS_IN comb2iq/M00_AXIS
+# 	clk /pll_0/clk_out1
+# 	reset_n rx_aresetn
+#     }
 
-    cell open-mri:user:CIC:1.0 cic_imag {
-	INP_DW 32
-	OUT_DW 32
-	RATE_DW 16
-	CIC_R 4095
-	CIC_N 6
-	CIC_M 1
-	PRUNE_BITS 0x480000004500000044000000440000004300000042000000410000003c00000031000000270000001c000000100000000400000000
-	VAR_RATE 1
-	EXACT_SCALING 0
-	PRG_SCALING 1
-    } {
-	S_AXIS_RATE rate/M01_AXIS
-	S_AXIS_IN comb2iq/M01_AXIS
-	clk /pll_0/clk_out1
-	reset_n rx_aresetn
-    }
+#     cell open-mri:user:CIC:1.0 cic_imag {
+# 	INP_DW 32
+# 	OUT_DW 32
+# 	RATE_DW 16
+# 	CIC_R 4095
+# 	CIC_N 6
+# 	CIC_M 1
+# 	PRUNE_BITS 0x480000004500000044000000440000004300000042000000410000003c00000031000000270000001c000000100000000400000000
+# 	VAR_RATE 1
+# 	EXACT_SCALING 0
+# 	PRG_SCALING 1
+#     } {
+# 	S_AXIS_RATE rate/M01_AXIS
+# 	S_AXIS_IN comb2iq/M01_AXIS
+# 	clk /pll_0/clk_out1
+# 	reset_n rx_aresetn
+#     }
 
-    cell xilinx.com:ip:axis_combiner:1.1 comb_iqmerge {
-	NUM_SI 2
-	TDATA_NUM_BYTES 4
-    } {
-	S00_AXIS cic_real/M_AXIS_OUT
-	S01_AXIS cic_imag/M_AXIS_OUT
-	aclk /pll_0/clk_out1
-	aresetn rx_aresetn
-    }
-} elseif {$dsp_source=="XILINX"} {
-    cell xilinx.com:ip:cic_compiler:4.0 cic_real {
+#     cell xilinx.com:ip:axis_combiner:1.1 comb_iqmerge {
+# 	NUM_SI 2
+# 	TDATA_NUM_BYTES 4
+#     } {
+# 	S00_AXIS cic_real/M_AXIS_OUT
+# 	S01_AXIS cic_imag/M_AXIS_OUT
+# 	aclk /pll_0/clk_out1
+# 	aresetn rx_aresetn
+#     }
+# } elseif {$dsp_source=="XILINX"} {
+#     cell xilinx.com:ip:cic_compiler:4.0 cic_real {
+# 	INPUT_DATA_WIDTH.VALUE_SRC USER
+# 	FILTER_TYPE Decimation
+# 	NUMBER_OF_STAGES 6
+# 	SAMPLE_RATE_CHANGES Programmable
+# 	MINIMUM_RATE 4
+# 	MAXIMUM_RATE 4095
+# 	FIXED_OR_INITIAL_RATE 512
+# 	RATESPECIFICATION Sample_Period
+# 	SAMPLEPERIOD 1
+# 	INPUT_DATA_WIDTH 32
+# 	QUANTIZATION Truncation
+# 	OUTPUT_DATA_WIDTH 32
+# 	USE_XTREME_DSP_SLICE true
+# 	HAS_DOUT_TREADY false
+# 	HAS_ARESETN true
+#     } {
+# 	S_AXIS_DATA comb2iq/M00_AXIS
+# 	S_AXIS_CONFIG rate/M00_AXIS
+# 	aclk /pll_0/clk_out1
+# 	aresetn rx_aresetn
+#     }
+
+#     cell xilinx.com:ip:cic_compiler:4.0 cic_imag {
+# 	INPUT_DATA_WIDTH.VALUE_SRC USER
+# 	FILTER_TYPE Decimation
+# 	NUMBER_OF_STAGES 6
+# 	SAMPLE_RATE_CHANGES Programmable
+# 	MINIMUM_RATE 4
+# 	MAXIMUM_RATE 4095
+# 	FIXED_OR_INITIAL_RATE 512
+# 	RATESPECIFICATION Sample_Period
+# 	SAMPLEPERIOD 1
+# 	INPUT_DATA_WIDTH 32
+# 	QUANTIZATION Truncation
+# 	OUTPUT_DATA_WIDTH 32
+# 	USE_XTREME_DSP_SLICE true
+# 	HAS_DOUT_TREADY false
+# 	HAS_ARESETN true
+#     } {
+# 	S_AXIS_DATA comb2iq/M01_AXIS
+# 	S_AXIS_CONFIG rate/M01_AXIS
+# 	aclk /pll_0/clk_out1
+# 	aresetn rx_aresetn
+#     }
+
+#     cell xilinx.com:ip:axis_combiner:1.1 comb_iqmerge {
+# 	NUM_SI 2
+# 	TDATA_NUM_BYTES 4
+#     } {
+# 	S00_AXIS cic_real/M_AXIS_DATA
+# 	S01_AXIS cic_imag/M_AXIS_DATA
+# 	aclk /pll_0/clk_out1
+# 	aresetn rx_aresetn
+#     }
+# } else {
+#     puts "Error: Unknown CIC source!"
+#     exit 1
+# }
+
+
+
+cell xilinx.com:ip:cic_compiler:4.0 cic_real {
 	INPUT_DATA_WIDTH.VALUE_SRC USER
 	FILTER_TYPE Decimation
 	NUMBER_OF_STAGES 6
@@ -130,17 +192,16 @@ if {$dsp_source=="OPENSOURCE"} {
 	RATESPECIFICATION Sample_Period
 	SAMPLEPERIOD 1
 	INPUT_DATA_WIDTH 32
-	QUANTIZATION Truncation
-	OUTPUT_DATA_WIDTH 32
+	QUANTIZATION Full_Precision
 	USE_XTREME_DSP_SLICE true
 	HAS_DOUT_TREADY false
 	HAS_ARESETN true
-    } {
+} {
 	S_AXIS_DATA comb2iq/M00_AXIS
 	S_AXIS_CONFIG rate/M00_AXIS
 	aclk /pll_0/clk_out1
 	aresetn rx_aresetn
-    }
+}
 
     cell xilinx.com:ip:cic_compiler:4.0 cic_imag {
 	INPUT_DATA_WIDTH.VALUE_SRC USER
@@ -148,33 +209,28 @@ if {$dsp_source=="OPENSOURCE"} {
 	NUMBER_OF_STAGES 6
 	SAMPLE_RATE_CHANGES Programmable
 	MINIMUM_RATE 4
-	MAXIMUM_RATE 4095
+	MAXIMUM_RATE 8192
 	FIXED_OR_INITIAL_RATE 512
 	RATESPECIFICATION Sample_Period
 	SAMPLEPERIOD 1
 	INPUT_DATA_WIDTH 32
-	QUANTIZATION Truncation
-	OUTPUT_DATA_WIDTH 32
+	QUANTIZATION Full_Precision
 	USE_XTREME_DSP_SLICE true
 	HAS_DOUT_TREADY false
 	HAS_ARESETN true
-    } {
+} {
 	S_AXIS_DATA comb2iq/M01_AXIS
 	S_AXIS_CONFIG rate/M01_AXIS
 	aclk /pll_0/clk_out1
 	aresetn rx_aresetn
-    }
+}
 
-    cell xilinx.com:ip:axis_combiner:1.1 comb_iqmerge {
+cell xilinx.com:ip:axis_combiner:1.1 comb_iqmerge {
 	NUM_SI 2
-	TDATA_NUM_BYTES 4
-    } {
+	TDATA_NUM_BYTES 14
+} {
 	S00_AXIS cic_real/M_AXIS_DATA
 	S01_AXIS cic_imag/M_AXIS_DATA
 	aclk /pll_0/clk_out1
 	aresetn rx_aresetn
-    }
-} else {
-    puts "Error: Unknown CIC source!"
-    exit 1
 }
